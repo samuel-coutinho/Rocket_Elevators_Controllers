7a0a44833c15366d2628bf41ed2d726e42cefc86
Samuel <sfcrp2000@gmail.com>
2020-10-01 12:46:44 -0400

    Update Residential_Controller.py
    Test scenarios working

--- Residential_Controller.py
+++ Residential_Controller.py
@@ -1,49 +1,6 @@
 import time
 
-#Main menu
-def main():
-    option1 = 0
-    print('Elevato 0 Floor = ', col1.elevatorList[0].currentFloor)
-    print('Elevator 1 Floor = ', col1.elevatorList[1].currentFloor)     
-    while True:    
-        while option1 != 3:
-            option1 = int(input("""Select from one of the following option:
-            1) Request a Elevator
-            2) Request a Floor
-            3) Exit)\n"""))
-            #Prevents numbers out of the range(1-3)
-            while option1 < 1 or option1 > 3:
-                option1 = int(input('Please provide a number between 1 and 3\n'))
-            #Option 1 of menu
-            if option1 == 1:
-                requestedFloor = int(input('Please provide the Floor you are located(1-10): '))
-                while requestedFloor < 1 or requestedFloor > 10:
-                    requestedFloor = int(input('Please provide a Floor between 1 and 10\n'))
-                requestedDirection = int(input('Please provide the Direction you want to go(1 - Up / 2 Down): '))
-                while requestedDirection < 1 or requestedDirection > 2:
-                    requestedDirection = int(input('Please provide a number between 1 and 2\n'))
-                if requestedDirection == 1:
-                    requestedDirection = 'Up'
-                if requestedDirection == 2:
-                    requestedDirection = 'Down'               
-                col1.requestElevator(requestedFloor, requestedDirection)
-            #Option 2 of menu
-            if option1 == 2:
-                elevator = int(input('Please provide the Elevator you are inside(0-1): '))
-                while elevator < 0 or elevator > 1:
-                    elevator = int(input('Please provide a number between 0 and 1\n'))
-                requestedFloor = int(input('Please provide the Floor you want to go(1-10): '))
-                while requestedFloor < 1 or requestedFloor > 10:
-                    requestedFloor = int(input('Please provide a Floor between 1 and 10\n'))
-                if requestedFloor == col1.elevatorList[elevator].currentFloor:
-                    print('You are already on the floor', requestedFloor)
-                    requestedFloor = int(input('Please provide the Floor you want to go(1-10): '))                                          
-                col1.requestFloor(elevator, requestedFloor)
-            print('Elevato 0 Floor = ', col1.elevatorList[0].currentFloor)
-            print('Elevator 1 Floor = ', col1.elevatorList[1].currentFloor)
-            
-        if option1 == 3:
-            break
+
 
 # Classes definition
 class Column:
@@ -53,21 +10,23 @@ class Column:
         self.numberOfElevators = numberOfElevators
         self.elevatorList = []
         self.callButtonList = []
-        self.createElevatorList()     
+        self.createElevatorList()
+        self.createcallButtonList()     
 
     def createElevatorList(self):
         for i in range(self.numberOfElevators):                             
             self.elevatorList.append(Elevator(i))
     
     def createcallButtonList(self):
-        for i in range(self.numberFloors):                             
+        for i in range(self.numberFloors + 1):                                       
             self.callButtonList.append(callButton(i))
 
     def requestElevator(self,requestedFloor, requestedDirection):          
         bestElevator = self.findBestElevator(requestedFloor, requestedDirection)      
         self.requestFloor(bestElevator.id, requestedFloor )                        
 
-    def requestFloor(self, elevator, requestedFloor):                                    
+    def requestFloor(self, elevator, requestedFloor):        
+        print('Closing doors')                                    
         if requestedFloor > self.elevatorList[elevator].currentFloor:
             elevatorDirection = 'Up'
         else:
@@ -115,30 +74,148 @@ class Elevator:
 
     def goToDestinationFloor(self, requestedFloor, elevatorDirection):
         self.destinationList.append(requestedFloor)
-        self.elevatorDirection = elevatorDirection        
-        self.destinationList.sort()
+        self.elevatorDirection = elevatorDirection 
+        if self.elevatorDirection == 'Up':       
+            self.destinationList.sort()
+        if self.elevatorDirection == 'Down':       
+            self.destinationList.sort(reverse=True)
         destination = self.destinationList[0]                                     
-        if self.currentFloor != destination:
+        while self.currentFloor != destination:
             print('Elevator = ', self.id)
             print('Current floor = ', self.currentFloor)
-            print('direction = ', self.elevatorDirection)  
+            print('Going ', self.elevatorDirection)  
             if self.elevatorDirection =='Up':
-                time.sleep(2)
+                time.sleep(1)
                 self.currentFloor += 1
             if self.elevatorDirection =='Down':
-                time.sleep(2)
+                time.sleep(1)
                 self.currentFloor -= 1                       
         self.destinationList = self.destinationList[1:]
         self.elevatorDirection = 'None'        
         print('Destination floor = ', self.currentFloor)
-        print('direction = ', self.elevatorDirection)
-        print('Opening Doors')        
+        #print('direction = ', self.elevatorDirection)
+        print('Opening Doors\n')            
+
 
 class callButton:
     def __init__(self, floor):    
         self.direction = 'None'
         self.floor = floor       
 
-#Initialization
-col1 = Column(10, 2)
-main()
+#Scenarios functions
+def Scenario1():
+    column1 = Column(10, 2)    
+    #Elevator A
+    column1.elevatorList[0].currentFloor = 2
+    column1.elevatorList[0].elevatorDirection = 'None'
+    #Elevator B
+    column1.elevatorList[1].currentFloor = 6
+    column1.elevatorList[1].elevatorDirection = 'None'
+    columnNumber = column1   
+
+    #Someone is on floor 3 and wants to go to the 7th floor.
+    floorOfPersonRequestingElevator = 3
+    requestedFloor = 7
+    print('Scenario 1')
+    print('Elevator A is Idle at floor 2')
+    print('Elevator B is Idle at floor 6')
+    print('Someone is on floor 3 and wants to go to the 7th floor.')
+    print('Elevator A = 0 is expected to be sent.\n')
+    runScenario(columnNumber, columnNumber.elevatorList[0].currentFloor, columnNumber.elevatorList[0].elevatorDirection, columnNumber.elevatorList[1].currentFloor, columnNumber.elevatorList[1].elevatorDirection, floorOfPersonRequestingElevator, requestedFloor)    
+
+def Scenario2():
+    column2 = Column(10, 2)
+    columnNumber = column2      
+    #Elevator A
+    columnNumber.elevatorList[0].currentFloor = 10
+    columnNumber.elevatorList[0].elevatorDirection = 'None'
+    #Elevator B
+    columnNumber.elevatorList[1].currentFloor = 3
+    columnNumber.elevatorList[1].elevatorDirection = 'None'  
+
+    #Someone is on the 1st floor and requests the 6th floor.
+    floorOfPersonRequestingElevator = 1
+    requestedFloor = 6
+    print('Scenario 2')
+    print('Elevator A is Idle at floor 10')
+    print('Elevator B is Idle at floor 3\n')
+    print('Someone is on the 1st floor and requests the 6th floor.')
+    print('Elevator B = 1 should be sent.\n')
+    runScenario(column2, column2.elevatorList[0].currentFloor, column2.elevatorList[0].elevatorDirection, column2.elevatorList[1].currentFloor, column2.elevatorList[1].elevatorDirection, floorOfPersonRequestingElevator, requestedFloor)    
+
+    # 2 minutes later, someone else is on the 3rd floor and requests the 5th floor. Elevator B should be sent
+    time.sleep(1)     
+    print('2 minutes later, someone else is on the 3rd floor and requests the 5th floor. Elevator B = 1 should be sent.\n')    
+    floorOfPersonRequestingElevator = 3
+    requestedFloor = 5
+    #(columnNumber, elevator A currentFloor, elevator A elevatorDirection, elevator B currentFloor, elevator B elevatorDirection, floorOfPersonRequestingElevator, requestedFloor) 
+    runScenario(columnNumber, columnNumber.elevatorList[0].currentFloor, columnNumber.elevatorList[0].elevatorDirection, columnNumber.elevatorList[1].currentFloor, columnNumber.elevatorList[1].elevatorDirection, floorOfPersonRequestingElevator, requestedFloor)    
+
+    #Finally, a third person is at floor 9 and wants to go down to the 2nd floor. Elevator A should be sent.
+    print('Finally, a third person is at floor 9 and wants to go down to the 2nd floor. Elevator A = 0 should be sent.\n')
+    floorOfPersonRequestingElevator = 9
+    requestedFloor = 2
+    time.sleep(1)
+    runScenario(column2, column2.elevatorList[0].currentFloor, column2.elevatorList[0].elevatorDirection, column2.elevatorList[1].currentFloor, column2.elevatorList[1].elevatorDirection, floorOfPersonRequestingElevator, requestedFloor)
+
+def Scenario3():
+    column3 = Column(10, 2)
+    columnNumber = column3     
+    #Elevator A
+    columnNumber.elevatorList[0].currentFloor = 10
+    columnNumber.elevatorList[0].elevatorDirection = 'None'
+    #Elevator B
+    columnNumber.elevatorList[1].currentFloor = 3
+    columnNumber.elevatorList[1].elevatorDirection = 'Up'
+    columnNumber.elevatorList[1].destinationList.append(6)    
+
+    #Someone is on floor 3 and wants to go to the 7th floor.
+    floorOfPersonRequestingElevator = 3
+    requestedFloor = 2
+    callButtonDirection = 'Up'
+    print('Scenario 3')
+    print('Elevator A is Idle at floor 10')
+    print('Elevator B is Moving from floor 3 to floor 6')
+    print('Someone is on floor 3 and requests the 2nd floor.')
+    print('Elevator A = 0 should be sent.\n')
+    #(columnNumber, elevator A currentFloor, elevator A elevatorDirection, elevator B currentFloor, elevator B elevatorDirection, floorOfPersonRequestingElevator, requestedFloor)
+    runScenario(columnNumber, columnNumber.elevatorList[0].currentFloor, columnNumber.elevatorList[0].elevatorDirection, columnNumber.elevatorList[1].currentFloor, columnNumber.elevatorList[1].elevatorDirection, floorOfPersonRequestingElevator, requestedFloor, callButtonDirection)
+
+    #5 minutes later, someone else is on the 10th floor and wants to go to the 3rd. Elevator B should be sent.
+    print('5 minutes later, someone else is on the 10th floor and wants to go to the 3rd.')
+    print('Elevator B should be sent.\n')
+    floorOfPersonRequestingElevator = 10
+    requestedFloor = 3
+    callButtonDirection = 'Down'
+    columnNumber.elevatorList[1].currentFloor = 6
+    columnNumber.elevatorList[1].elevatorDirection = 'None'
+    columnNumber.elevatorList[1].destinationList = columnNumber.elevatorList[1].destinationList[1:]
+
+    runScenario(columnNumber, columnNumber.elevatorList[0].currentFloor, columnNumber.elevatorList[0].elevatorDirection, columnNumber.elevatorList[1].currentFloor, columnNumber.elevatorList[1].elevatorDirection, floorOfPersonRequestingElevator, requestedFloor, callButtonDirection)
+
+
+def runScenario(columnNumber, elevatorAinitialFloor, elevatorAinitialDirection, elevatorBinitialFloor, elevatorBinitialDirection, floor, requestedFloor, callButtonDirection ):        
+      
+        #Elevator A
+        columnNumber.elevatorList[0].currentFloor = elevatorAinitialFloor
+        columnNumber.elevatorList[0].elevatorDirection = elevatorAinitialDirection
+        #Elevator B
+        columnNumber.elevatorList[1].currentFloor = elevatorBinitialFloor
+        columnNumber.elevatorList[1].elevatorDirection = elevatorBinitialDirection        
+
+        #Request Elevator        
+        columnNumber.callButtonList[floor].direction = callButtonDirection    
+        columnNumber.requestElevator(floor, columnNumber.callButtonList[floor].direction)  
+        print('Please wait a moment\n') 
+        time.sleep(2) 
+    
+        #Request floor        
+        print('Going to floor ',requestedFloor,'\n')
+        elevator = columnNumber.findBestElevator(floor, columnNumber.callButtonList[floor].direction)    
+        columnNumber.requestFloor(elevator.id, requestedFloor)
+        time.sleep(2)
+#-------Test Section-------#
+#Scenario1()
+#Scenario2()
+#Scenario3()
+
